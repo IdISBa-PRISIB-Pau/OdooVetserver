b10c2b4b1289c76b60bdc0644e424a2880664694
unknown <pericassim20002@hotmail.com>
2023-09-15 09:56:55 +0200

    eval tree

--- veterinary/models/appointment.py
+++ veterinary/models/appointment.py
@@ -50,12 +50,6 @@ class Appointment(models.Model):
             target_date_tz_dt = fields.datetime.context_timestamp(cr, uid, target_date_utc_dt, context=context)
             res[issue.id] = target_date_tz_dt.strftime(DEFAULT_SERVER_DATETIME_FORMAT)           
         return res
-
-    _columns = {
-        'Fecha de la cita': fields.function(
-            _compute_target_date_tz, type="datetime",
-            string="Fecha corregida por timezone para los emails",
-            )
         }
 
     @api.model
